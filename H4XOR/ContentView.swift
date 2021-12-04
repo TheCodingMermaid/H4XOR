3aaf4055489a952761c8056a3e5ac1be974c3879
jaz <jazpilates@gmail.com>
2021-12-04 16:02:45 +0100

    Initial Commit

---
+++ H4XOR/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4XOR
+//
+//  Created by Jaz on 04.12.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ H4XOR/H4XORApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XORApp.swift
+//  H4XOR
+//
+//  Created by Jaz on 04.12.21.
+//
+
+import SwiftUI
+
+@main
+struct H4XORApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
